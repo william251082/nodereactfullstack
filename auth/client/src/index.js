f2a445c1b0a48f9a8f5e29e97b6a90b204b44837
william <william.del.rosario@iceshop.nl>
2020-03-07 12:13:36 +0100

    including redux

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -4,13 +4,18 @@ import App from "./components/App";
 import {BrowserRouter, Route} from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducers from './reducers'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App>
-            <Route path="/" exact component={Welcome}/>
-            <Route path="/signup" component={Signup}/>
-        </App>
-    </BrowserRouter>,
+    <Provider store={createStore(reducers, {})}>
+        <BrowserRouter>
+            <App>
+                <Route path="/" exact component={Welcome}/>
+                <Route path="/signup" component={Signup}/>
+            </App>
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 );
