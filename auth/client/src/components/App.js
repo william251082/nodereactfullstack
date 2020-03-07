ffa1f5d5b732dfe400b9e099c41b2653c3daefb7
william <william.del.rosario@iceshop.nl>
2020-03-07 11:46:01 +0100

    creating header --don't use Link without Router

--- auth/client/src/components/App.js
+++ auth/client/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from "./Header";
 
 export default () => {
     return (
         <div>
-            HI
+            <Header />
+                HI
         </div>
-    )
-}
+    );
+};
