17a54ec29f79d98d5ac6065993d24fe08ac290d6
william <william.del.rosario@iceshop.nl>
2020-03-07 12:01:04 +0100

    scaffolding the signup form

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import App from "./components/App";
 import {BrowserRouter, Route} from "react-router-dom";
 import Welcome from "./components/Welcome";
+import Signup from "./components/auth/Signup";
 
 ReactDOM.render(
     <BrowserRouter>
         <App>
             <Route path="/" exact component={Welcome}/>
+            <Route path="/signup" component={Signup}/>
         </App>
     </BrowserRouter>,
     document.querySelector('#root')
