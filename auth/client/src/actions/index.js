1b1854fa720ded36a88a2d15bf912c040a4edd9d
william <william.del.rosario@iceshop.nl>
2020-03-08 10:14:21 +0100

    dispatching actions

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import {AUTH_USER} from "./types";
 
 export const signup = (formProps) => dispatch => {
-    axios.post('http://localhost:3090/signup', formProps);
+    const response = axios.post('http://localhost:3090/signup', formProps);
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
 };
