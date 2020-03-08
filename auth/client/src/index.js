da3e604b7720b22949194436bfbb739bc716b078
william <william.del.rosario@iceshop.nl>
2020-03-08 10:40:20 +0100

    used requireAuth HOC

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from "./components/App";
 import {BrowserRouter, Route} from "react-router-dom";
 import Welcome from "./components/Welcome";
+import Feature from "./components/Feature";
 import Signup from "./components/auth/Signup";
 import {Provider} from "react-redux";
 import {applyMiddleware, createStore} from "redux";
@@ -17,6 +18,7 @@ ReactDOM.render(
             <App>
                 <Route path="/" exact component={Welcome}/>
                 <Route path="/signup" component={Signup}/>
+                <Route path="/feature" component={Feature}/>
             </App>
         </BrowserRouter>
     </Provider>,
